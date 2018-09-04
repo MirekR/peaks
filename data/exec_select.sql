0dbaf6a683ff1cfddf19dffdf6b6ff089aef66ba
Zuzana Brabcova <zbrabcov@r25-zbra-m-l.home>
2018-09-05 00:03:03 +0100

    nelibil se mi koment, kde byl

--- data/exec_select.sql
+++ data/exec_select.sql
@@ -1,11 +1,11 @@
-with tolerance_tab as (select 5 /* this number can be change to whatever to reflect desired tolerance*/ as tol) 
-select t1.* 
-  from peak.chrom t1 
-  where exists (select 1 
-  				from peak.chrom t2  
+with tolerance_tab as (select 5 as tol) /* this number can be change to whatever to reflect desired tolerance*/ 
+select t1.*
+  from peak.chrom t1
+  where exists (select 1
+  				from peak.chrom t2
   				join tolerance_tab t
-  				where t2.chrom = t1.chrom  
-  				and t2.start between t1.start - t.tol and t1.start + t.tol 
-  				and t2.end between t1.end - t.tol and t1.end + t.tol 
-  				and t1.file_name != t2.file_name) 
+  				where t2.chrom = t1.chrom
+  				and t2.start between t1.start - t.tol and t1.start + t.tol
+  				and t2.end between t1.end - t.tol and t1.end + t.tol
+  				and t1.file_name != t2.file_name)
   order by chrom asc, start asc, end asc;
