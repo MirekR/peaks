793a829528ffe2edac684e1b40395f5f28d7b3a8
mirekrousal@centrum.cz <mirekrousal@centrum.cz>
2018-09-04 22:56:49 +0100

    Temporary fix for gsed on path

--- prepareData.sh
+++ prepareData.sh
@@ -2,7 +2,7 @@
 
 mkdir -p docker-data
 
-find ./data -type f -name "S*.txt" -print0 | xargs -0 sed -i '/^#/ d'
-find ./data -type f -name "S*.txt" -print0 | xargs -0 sed -i '/^$/d'
+find ./data -type f -name "S*.txt" -print0 | xargs -0 gsed -i '/^#/ d'
+find ./data -type f -name "S*.txt" -print0 | xargs -0 gsed -i '/^$/d'
 
 ls -1 ./data/S*PE_tabular.txt  | sed 's/\.[a-z]*//g' | xargs Rscript scripts/generateInserts.R
